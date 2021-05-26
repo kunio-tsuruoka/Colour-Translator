881ab44aaabf5b10aabcc4908a45eb507c8acdfa
Kunio Tsuruoka <47134510+kunio-tsuruoka@users.noreply.github.com>
2021-05-27 00:12:33 +0900

    index
    this is a gas script
    this is not a Javascript file, which is why I did not use any arrow functions.

---
+++ index.js
@@ -0,0 +1,42 @@
+
+function main () {
+  const colors = getHexAndLabel();
+  const translatedColors = translateColors(colors)
+  translatedColors.forEach(e => {writeColors(e.hex,e.name)})
+}
+function translateColors(colors){
+  const result = colors.map(e => {
+    const translatedLabel = translateLabel(e.name)
+    console.log(translatedLabel)
+    return {
+      hex:e.hex,
+      name: translatedLabel
+      }
+    });
+  return result;
+}
+function getHexAndLabel() {
+    const json = UrlFetchApp.fetch("https://unpkg.com/color-name-list@8.11.0/dist/colornames.json");
+    // json形式で返ってくるのでパース
+    const array = JSON.parse(json);
+    return array;
+}
+
+function translateLabel(label){
+  const sourceLanguage = "en";
+  const targetLanguage = "ja";
+  const output = LanguageApp.translate(label, sourceLanguage, targetLanguage);
+  Utilities.sleep(1000)
+  return output;
+}
+
+function writeColors(hex,label)
+{
+ const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('色');
+ const values = sheet.getDataRange().getValues();
+ const col = values.length - 1
+ const row = values[col].length -1 
+ let temp = [hex,label]
+ values.splice(col + 1,0,temp) 
+ sheet.getRange(1,1, values.length, values[0].length).setValues(values);
+}
